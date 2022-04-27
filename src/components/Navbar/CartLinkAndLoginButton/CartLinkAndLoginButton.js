52f2a684444e618bfb16f522a88edfb93cceea30
David Jacimovic <me@davidjacimovic.com>
2022-04-27 12:17:38 +0200

    cart link and login button added

--- src/components/Navbar/CartLinkAndLoginButton/CartLinkAndLoginButton.js
+++ src/components/Navbar/CartLinkAndLoginButton/CartLinkAndLoginButton.js
@@ -1,10 +1,23 @@
 import React from "react";
 import styles from "./CartLinkAndLoginButton.module.css";
+import { FaShoppingCart, FaUserPlus } from "react-icons/fa";
+import { Link } from "react-router-dom";
 
 const CartLinkAndLoginButton = () => {
   return (
-    <div>
-      <h2>CartAndLogin</h2>
+    <div className={styles.cart_btn_container}>
+      <Link to="/cart" className={styles.cart_btn}>
+        Cart
+        <span className={styles.cart_container}>
+          <FaShoppingCart className={styles.cart_icon} />
+          <span className={styles.cart_value}>
+            <p>12</p>
+          </span>
+        </span>
+      </Link>
+      <button type="button" className={styles.auth_btn}>
+        Login <FaUserPlus className={styles.login_icon} />
+      </button>
     </div>
   );
 };
